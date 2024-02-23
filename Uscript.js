7b17538e06a922e42763f5e413701f22df7b0f54
Tamal Barman <98608183+tamalbarman@users.noreply.github.com>
2024-02-23 19:16:12 +0530

    Updated portfolio

---
+++ Uscript.js
@@ -0,0 +1,34 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+// var sidemenu = document.getElementById("sidemenu");
+
+// // function openmenu() {
+// //   sidemenu.style.right = "0";
+// // }
+// // function closemenu() {
+// //   sidemenu.style.right = "-200px";
+// // }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Messege sent sucessfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
